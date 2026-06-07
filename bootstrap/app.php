8918c8e92e27465094852a66b946df6541fd2647
Aya Khalil <ayakhalil545@gmail.com>
2026-06-07 11:11:55 +0300

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,15 @@ use App\Http\Middleware\Admin;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
+
+         api: [
+            __DIR__ . '/../routes/api_delivery.php',
+             __DIR__ . '/../routes/api_user.php',
+            // __DIR__ . '/../routes/api_admin.php',
+            //  __DIR__ . '/../routes/api_landing.php',
+        ],
+
+     
         commands: __DIR__ . '/../routes/console.php',
         channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
